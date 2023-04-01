90afb9d82724f2770d92f2750d94dd482819cbc5
Gabi Boss <Gabi Boss@DESKTOP-3V0HLL6>
2023-04-01 23:31:40 +0300

    Add project files.

---
+++ test_project/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+        
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("unu");
+                    break;
+                case 2:
+                    Console.WriteLine("doi");
+                    break;
+                case 3:
+                    Console.WriteLine("trei");
+                    break;
+                case 5:
+                    Console.WriteLine("cinci");
+                    break;
+                case 8:
+                    Console.WriteLine("opt");
+                    break;
+                default:
+                    Console.WriteLine("Neidentificat");
+                    break;
+
+
+
+            }
+        }
+    }
+}
